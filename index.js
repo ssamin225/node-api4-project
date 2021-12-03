92cccedc01221b36b06961e4a3afe216ef843a07
ssamin225 <sadmansamin225@gmail.com>
2021-12-02 22:07:12 -0500

    wip

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
+const path = require('path')
 const express = require('express');
 
 const server = express();
 
 server.use(express.json());
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 server.get('/api/users', (req, res) => {
   res.status(200).json([
@@ -11,6 +13,10 @@ server.get('/api/users', (req, res) => {
     {id: 2, username: 'bar'},
     {id: 3, username: 'foobar'}
   ])
+})
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/name', 'index.html'))
 })
 
 server.post('/api/register', (req, res) => {
